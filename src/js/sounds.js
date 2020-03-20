ff104896bac4db645b9efcff1bbdc600d27d1dee
Albert Lee <aclee94@gmail.com>
2020-03-20 17:02:50 -0400

    Add sounds singleton

---
+++ src/js/sounds.js
@@ -0,0 +1,17 @@
+class Sounds {
+  initialize() {
+    this.muted = true;
+    this.backgroundMusic = new Audio("./src/sounds/sw.mp3");
+    this.backgroundMusic.volume = 0.1;
+    this.backgroundMusic.loop = true;
+
+    this.impact = new Audio("./src/sounds/impact.mp3");
+    this.laserSound = new Audio("./src/sounds/Digital_SFX_Set/laser1.mp3");
+    this.enemyLaserSound = new Audio("./src/sounds/tie_fire_laser.mp3");
+    this.upgradeSound = new Audio("./src/sounds/upgrade_complete.mp3");
+    this.tieExplodeSound = new Audio("./src/sounds/tie_explode_short.mp3");
+    this.tieExplodeSound.volume = 0.1;
+  }
+}
+
+export default new Sounds();
