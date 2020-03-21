c4d4a6592b34b0c4d9ed7d5922a433ef082cadf5
Albert Lee <aclee94@gmail.com>
2020-03-20 23:50:39 -0400

    Add collision function

--- src/js/moving_object.js
+++ src/js/moving_object.js
@@ -15,6 +15,58 @@ class MovingObject extends GameCanvas {
     this.posX = settings.posX;
     this.posY = settings.posY;
   }
+
+  checkCollision(object) {
+    let object1 = this;
+    let object2 = object;
+    switch (object1.name) {
+      case "laser1":
+      case "laser2":
+      case "laser3":
+      case "laser4":
+      case "laser5":
+        if (!object1 || !object2) {
+          return false;
+        } else if (
+          object1.posY > object2.posY + object2.height ||
+          object1.posY < object2.posY ||
+          object1.posX + 10 < object2.posX ||
+          object1.posX > object2.posX + object2.width
+        ) {
+          return false;
+        } else {
+          return true;
+        }
+
+      case "red-laser":
+        if (!object1 || !object2) {
+          return false;
+        } else if (
+          object1.posY > object2.y + object2.height ||
+          object1.posY < object2.y ||
+          object1.posX + 10 < object2.x ||
+          object1.posX > object2.x + object2.width
+        ) {
+          return false;
+        } else {
+          return true;
+        }
+
+      case "player":
+        if (!object1 || !object2) {
+          return false;
+        } else if (
+          object1.y > object2.posY + object2.height ||
+          object1.y + object1.height < object2.posY ||
+          object1.x + object1.width < object2.posX ||
+          object1.x > object2.posX + object2.width
+        ) {
+          return false;
+        } else {
+          return true;
+        }
+    }
+  }
 }
 
 export default MovingObject;
