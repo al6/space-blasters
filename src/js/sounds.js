37fd82438057c10cc394d9892b5bf84e06261e53
Albert Lee <aclee94@gmail.com>
2020-03-21 00:11:03 -0400

    Adjust volumes

--- src/js/sounds.js
+++ src/js/sounds.js
@@ -6,11 +6,14 @@ class Sounds {
     this.backgroundMusic.loop = true;
 
     this.impact = new Audio("./src/sounds/impact.mp3");
+    this.impact.volume = 0.5;
+
     this.laserSound = new Audio("./src/sounds/Digital_SFX_Set/laser1.mp3");
     this.enemyLaserSound = new Audio("./src/sounds/tie_fire_laser.mp3");
     this.upgradeSound = new Audio("./src/sounds/upgrade_complete.mp3");
+
     this.tieExplodeSound = new Audio("./src/sounds/tie_explode_short.mp3");
-    this.tieExplodeSound.volume = 0.1;
+    this.tieExplodeSound.volume = 0.2;
   }
 }
 
