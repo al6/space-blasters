8e32ca66ba18f8dbe86917b986e79e10b77adefd
Albert Lee <aclee94@gmail.com>
2020-03-20 23:49:04 -0400

    Remove unused code

--- src/js/game_canvas.js
+++ src/js/game_canvas.js
@@ -2,8 +2,6 @@ class GameCanvas {
   constructor() {
     this.canvas = document.getElementById("game-canvas");
     this.context = this.canvas.getContext("2d");
-    this.playingStatus = document.getElementById("sound");
-    this.notPlayingStatus = document.getElementById("no-sound");
   }
 }
 
