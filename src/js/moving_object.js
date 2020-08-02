4128fc7296f82a63fc10abbbcc6c86109a447c65
Albert Lee <aclee94@gmail.com>
2020-08-01 21:20:01 -0400

    dry up code a bit and write helpful comments describing each collision scenario

--- src/js/moving_object.js
+++ src/js/moving_object.js
@@ -7,7 +7,7 @@ class MovingObject extends GameCanvas {
       velocityX: 0,
       velocityY: 0,
       posX: 0,
-      posY: 0
+      posY: 0,
     };
     const settings = Object.assign({}, defaults, props);
     this.velocityX = settings.velocityX;
@@ -19,52 +19,53 @@ class MovingObject extends GameCanvas {
   checkCollision(object) {
     let object1 = this;
     let object2 = object;
-    switch (object1.name) {
-      case "laser1":
-      case "laser2":
-      case "laser3":
-      case "laser4":
-      case "laser5":
-        if (!object1 || !object2) {
-          return false;
-        } else if (
-          object1.posY > object2.posY + object2.height ||
-          object1.posY < object2.posY ||
-          object1.posX + 10 < object2.posX ||
-          object1.posX > object2.posX + object2.width
-        ) {
-          return false;
-        } else {
-          return true;
-        }
+    if (!object1 || !object2) {
+      return false;
+    } else {
+      switch (object1.name) {
+        case "laser1":
+        case "laser2":
+        case "laser3":
+        case "laser4":
+        case "laser5":
+          if (
+            // player laser misses enemy ship
+            object1.posY > object2.posY + object2.height ||
+            object1.posY < object2.posY ||
+            object1.posX + 10 < object2.posX ||
+            object1.posX > object2.posX + object2.width
+          ) {
+            return false;
+          } else {
+            return true;
+          }
 
-      case "red-laser":
-        if (!object1 || !object2) {
-          return false;
-        } else if (
-          object1.posY > object2.y + object2.height ||
-          object1.posY < object2.y ||
-          object1.posX + 10 < object2.x ||
-          object1.posX > object2.x + object2.width
-        ) {
-          return false;
-        } else {
-          return true;
-        }
+        case "red-laser":
+          if (
+            // enemy laser misses player ship
+            object1.posY > object2.y + object2.height ||
+            object1.posY < object2.y ||
+            object1.posX + 10 < object2.x ||
+            object1.posX > object2.x + object2.width
+          ) {
+            return false;
+          } else {
+            return true;
+          }
 
-      case "player":
-        if (!object1 || !object2) {
-          return false;
-        } else if (
-          object1.y > object2.posY + object2.height ||
-          object1.y + object1.height < object2.posY ||
-          object1.x + object1.width < object2.posX ||
-          object1.x > object2.posX + object2.width
-        ) {
-          return false;
-        } else {
-          return true;
-        }
+        case "player":
+          if (
+            // player misses upgrade sphere
+            object1.y > object2.posY + object2.height ||
+            object1.y + object1.height < object2.posY ||
+            object1.x + object1.width < object2.posX ||
+            object1.x > object2.posX + object2.width
+          ) {
+            return false;
+          } else {
+            return true;
+          }
+      }
     }
   }
 }
